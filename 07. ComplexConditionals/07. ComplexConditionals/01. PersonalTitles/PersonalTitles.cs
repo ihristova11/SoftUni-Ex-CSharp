ecb3c4fd635b1b035d24339268187c228e69cfa3
Irina Hristova <hristova119@gmail.com>
2017-08-04 12:25:40 +0300

    CSharp-Exercises
    Some exercises.

---
+++ 07. ComplexConditionals/07. ComplexConditionals/01. PersonalTitles/PersonalTitles.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class PersonalTitles
+{
+    static void Main()
+    {
+        double age = double.Parse(Console.ReadLine());
+        string gender = Console.ReadLine();
+        if (age < 16)
+        {
+            switch(gender)
+            {
+                case "m": Console.WriteLine("Master");
+                    break;
+                case "f": Console.WriteLine("Miss");
+                    break;
+            }
+        }
+        else
+        {
+            switch (gender)
+            {
+                case "m":
+                    Console.WriteLine("Mr.");
+                    break;
+                case "f":
+                    Console.WriteLine("Ms.");
+                    break;
+            }
+        }
+    }
+}
+
---
+++ 07. ComplexConditionals/07. ComplexConditionals/02. SmallShop/SmallShop.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+class SmallShop
+{
+    static void Main()
+    {
+        string product = Console.ReadLine();
+        string city = Console.ReadLine();
+        double quantity = double.Parse(Console.ReadLine());
+        if (city == "Sofia")
+        {
+            switch(product)
+            {
+                case "coffee": Console.WriteLine(0.5 * quantity);
+                    break;
+                case "water": Console.WriteLine(0.8 * quantity);
+                    break;
+                case "beer": Console.WriteLine(1.2 * quantity);
+                    break;
+                case "sweets": Console.WriteLine(1.45 * quantity);
+                    break;
+                case "peanuts": Console.WriteLine(1.6 * quantity);
+                    break;
+            }
+        }
+        else if (city == "Plovdiv")
+        {
+            switch (product)
+            {
+                case "coffee":
+                    Console.WriteLine(0.4 * quantity);
+                    break;
+                case "water":
+                    Console.WriteLine(0.7 * quantity);
+                    break;
+                case "beer":
+                    Console.WriteLine(1.15 * quantity);
+                    break;
+                case "sweets":
+                    Console.WriteLine(1.3 * quantity);
+                    break;
+                case "peanuts":
+                    Console.WriteLine(1.5 * quantity);
+                    break;
+            }
+        }
+        else if (city == "Varna")
+        {
+            switch (product)
+            {
+                case "coffee":
+                    Console.WriteLine(0.45 * quantity);
+                    break;
+                case "water":
+                    Console.WriteLine(0.7 * quantity);
+                    break;
+                case "beer":
+                    Console.WriteLine(1.1 * quantity);
+                    break;
+                case "sweets":
+                    Console.WriteLine(1.35 * quantity);
+                    break;
+                case "peanuts":
+                    Console.WriteLine(1.55 * quantity);
+                    break;
+            }
+        }
+    }
+}
+
